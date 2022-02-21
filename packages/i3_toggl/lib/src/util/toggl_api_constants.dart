f3293ff359f56cd6f39788009dd100a70ee09b75
polydome <polydome@outlook.com>
2022-02-21 20:01:43 +0100

    Fix api key constant not being red properly

--- packages/i3_toggl/lib/src/util/toggl_api_constants.dart
+++ packages/i3_toggl/lib/src/util/toggl_api_constants.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
 const togglApiUrl = "https://api.track.toggl.com/api/v8";
-const togglApiKey = String.fromEnvironment("TOGGL_API_KEY");
+// TODO: make constant
+final togglApiKey = Platform.environment["TOGGL_API_KEY"]!;
 const sessionCookieKey = "__Host-timer-session";
