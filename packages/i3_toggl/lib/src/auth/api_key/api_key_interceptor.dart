c5bd4da188a9713fabf9909b76a03702c9d61dd4
polydome <polydome@outlook.com>
2022-02-21 19:30:20 +0100

    Fix api key interceptor failing

--- packages/i3_toggl/lib/src/auth/api_key/api_key_interceptor.dart
+++ packages/i3_toggl/lib/src/auth/api_key/api_key_interceptor.dart
@@ -1,15 +1,21 @@
 import 'package:dio/dio.dart';
+import 'package:i3_toggl/src/util/toggl_api_constants.dart';
 
 import '../../util/http_util.dart';
 
 class ApiKeyAuthInterceptor extends Interceptor {
-  final String _apiKey;
-
-  ApiKeyAuthInterceptor(this._apiKey);
+  ApiKeyAuthInterceptor();
   
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final credentials = encodeCredentials(_apiKey, 'api_token');
+    if (togglApiKey.isEmpty) {
+      final error = StateError("TOGGL_API_KEY env variable is empty.");
+      handler.reject(DioError(requestOptions: options, error: error));
+      return;
+    }
+
+    final credentials = encodeCredentials(togglApiKey, 'api_token');
     options.headers['Authorization'] = 'Basic $credentials';
+    handler.next(options);
   }
 }
--- packages/i3_toggl/lib/src/util/toggl_api_constants.dart
+++ packages/i3_toggl/lib/src/util/toggl_api_constants.dart
@@ -1,3 +1,3 @@
 const togglApiUrl = "https://api.track.toggl.com/api/v8";
-const togglApiKey = String.fromEnvironment("API_KEY");
+const togglApiKey = String.fromEnvironment("TOGGL_API_KEY");
 const sessionCookieKey = "__Host-timer-session";
